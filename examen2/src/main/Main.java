781c6e4ac35e48723aa87903a8d4f97361245de8
Isabel <isagaleano9@gmail.com>
2022-04-26 18:35:05 -0600

    Add mysql

--- examen2/src/main/Main.java
+++ examen2/src/main/Main.java
@@ -20,13 +20,13 @@ public class Main {
         Connection conn2 = null;
         Statement stmt2 = null;
         ResultSet rs2 = null;
+        String idUsuario = "1";
         String connectionUrl2 = "jdbc:mysql://localhost/proyectoFinal?user=root&password=p@ssw0rd1&useSSL=false";
         String query2 = "SELECT idUsuarioF, idReto, nombreHito FROM avanceUsuarioFReto WHERE idUsuarioF = " + idUsuario;
         conn2 = DriverManager.getConnection(connectionUrl2);
         stmt2 = conn2.createStatement();
         rs2 = stmt2.executeQuery(query2);
-        while(rs2.next())
-        {
+        while (rs2.next()) {
 //            System.out.println(rs2.);
 //            Avance tmpAvance = new Avance(rs.getInt("idUsuarioF"), rs.getInt("idReto"), rs.getString("nombreHito"));
 //            avances.add(tmpAvance);
