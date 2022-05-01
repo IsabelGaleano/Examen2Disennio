e378bc88445fa4d103d4ae8119d170b9ab347278
Allan Rodriguez Venegas <arodriguezv@ucenfotec.ac.cr>
2022-04-30 18:42:52 -0600

    Correccion reporte 4

--- examen2/src/services/BitacoraService.java
+++ examen2/src/services/BitacoraService.java
@@ -62,30 +62,57 @@ public class BitacoraService {
                     break;
 
                 case PENDIENTES_Y_DIAS:
-                    query = "select c.nombre,c.apellido,\n" +
-                            " TIMESTAMPDIFF(DAY,(select b.fecha from bitacoras b where b.idCliente = c.id and status = 'PAGO_PENDIENTE'),sysdate()) AS dias_transcurridos\n" +
+                    query = "select v.nombre,v.apellido,v.pago_pendiente,v.compro,TIMESTAMPDIFF(DAY,v.pago_pendiente, IFNULL(v.compro,sysdate())) AS dias_transcurridos\n" +
+                            "from (\n" +
+                            "select c.nombre,c.apellido,\n" +
+                            "(select b.fecha from bitacoras b where b.idCliente = c.id and status = 'PAGO_PENDIENTE') as pago_pendiente,\n" +
+                            "(select b.fecha from bitacoras b where b.idCliente = c.id and status = 'COMPRÓ') as compro\n" +
                             "from clientes c \n" +
-                            "where c.status = 'PAGO_PENDIENTE' \n";
+                            "where c.id in (select b.idCliente from bitacoras b where  status = 'PAGO_PENDIENTE')) as v;";
                     resultados = accesoBD.ejecutarSQL(query);
                     while (resultados.next()) {
                         String  nombre = resultados.getString("nombre");
                         String  apellido = resultados.getString("apellido");
+                        Date pago_pendiente = resultados.getDate("pago_pendiente");
+                        Date compro = resultados.getDate("compro");
                         int dias_transcurridos = resultados.getInt("dias_transcurridos");
 
-                        retornar +=" El cliente: " + nombre +" " + apellido  + " tiene " + dias_transcurridos + " días, pendientes de pago \n " ;
+                        retornar +=" El cliente: " + nombre +" " + apellido  + " tiene " + dias_transcurridos + " días, pendientes de pago, fecha pago pendiente: "+ pago_pendiente + " fecha compra: " + compro + " \n " ;
                     }
                     break;
                 case TIEMPO_SEGUIMIENTO_Y_POSTERIORES:
-                    query = "select c.nombre,c.apellido,\n" +
-                            " TIMESTAMPDIFF(DAY, c.creado,(select b.fecha from bitacoras b where b.idCliente = c.id and status = 'SEGUIMIENTO')) AS dias_transcurridos\n" +
-                            "from clientes c ";
+                    query = "select v.nombre,v.apellido,v.nuevo,v.seguimiento,v.pendiente,v.pago_pendiente,v.compro,\n" +
+                            "TIMESTAMPDIFF(DAY, nuevo,seguimiento) AS dias_segumiento,\n" +
+                            "TIMESTAMPDIFF(DAY, seguimiento,pendiente) AS dias_pendiente,\n" +
+                            "TIMESTAMPDIFF(DAY, seguimiento,pago_pendiente) AS dias_pago_pendiente,\n" +
+                            "TIMESTAMPDIFF(DAY, pago_pendiente,compro) AS dias_compra\n" +
+                            "from (\n" +
+                            "select c.nombre,c.apellido,\n" +
+                            "(select b.fecha from bitacoras b where b.idCliente = c.id and status = 'NUEVO') as nuevo,\n" +
+                            "(select b.fecha from bitacoras b where b.idCliente = c.id and status = 'SEGUIMIENTO') as seguimiento,\n" +
+                            "(select b.fecha from bitacoras b where b.idCliente = c.id and status = 'PENDIENTE') as pendiente,\n" +
+                            "(select b.fecha from bitacoras b where b.idCliente = c.id and status = 'PAGO_PENDIENTE') as pago_pendiente,\n" +
+                            "(select b.fecha from bitacoras b where b.idCliente = c.id and status = 'COMPRÓ') as compro\n" +
+                            "from clientes c \n" +
+                            "where c.id in (select b.idCliente from bitacoras b where  status = 'SEGUIMIENTO'))v;";
                     resultados = accesoBD.ejecutarSQL(query);
                     while (resultados.next()) {
                         String  nombre = resultados.getString("nombre");
                         String  apellido = resultados.getString("apellido");
-                        int dias_transcurridos = resultados.getInt("dias_transcurridos");
+                        Date nuevo = resultados.getDate("nuevo");
+                        Date seguimiento = resultados.getDate("seguimiento");
+                        Date pendiente = resultados.getDate("pendiente");
+                        Date pago_pendiente = resultados.getDate("pago_pendiente");
+                        Date compro    = resultados.getDate("compro");
+                        int dias_segumiento = resultados.getInt("dias_segumiento");
+                        int dias_pendiente = resultados.getInt("dias_pendiente");
+                        int dias_pago_pendiente = resultados.getInt("dias_pago_pendiente");
+                        int dias_compra = resultados.getInt("dias_compra");
 
-                        retornar +=" El cliente: " + nombre +" " + apellido  + " tiene " + dias_transcurridos + " días, hasta el segumiento \n " ;
+                        retornar +=" El cliente: " + nombre +" " + apellido  + " tiene los siguientes  registros de seguimiento: "+
+                                 "Creado:" + nuevo + " Seguimiento: " + seguimiento + " pendiente de compra: " + pendiente + " pago pendiente:" + pago_pendiente + " Compra: "+ compro +
+                                 " han pasado " + dias_segumiento + " días, hasta el seguimiento, "+ dias_pendiente + " en estado pendiente, " + dias_pago_pendiente + " en estado pendiente de pago,"+
+                                 dias_compra +" hasta la compra" +"\n " ;
                     }
                     break;
             }
